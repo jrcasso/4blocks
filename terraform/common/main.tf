0bc6f7fe0b9e75880927b75d223f2eee26492aa8
Justin Casso <justincasso1@gmail.com>
2022-01-17 05:44:35 +0000

    Add terraform config for application ELB DNS

---
+++ terraform/common/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-applications"
+    key    = "4blocks/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+}
+
+data "terraform_remote_state" "operation" {
+  backend = "s3"
+  config = {
+    bucket = "terraform-operations"
+    key    = "${var.operation}/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_route53_record" "elb_dns" {
+  zone_id = data.terraform_remote_state.operation.outputs.hosted_zone.id
+  name    = "4blocks.${var.operation}.${var.organization}.com."
+  type    = "CNAME"
+  ttl     = "60"
+  records = ["aee447eb5236f4e6c967260acfedbf3c-1435398619.us-east-1.elb.amazonaws.com"]
+}
---
+++ terraform/common/variables.tf
@@ -0,0 +1,9 @@
+variable "organization" {
+  type        = string
+  description = "Organization name. Must be DNS-compliant."
+}
+
+variable "operation" {
+  type        = string
+  description = "Operation name; prototypical scopes include dev, stage, and production 'environments'. Must be DNS-compliant."
+}
